4b80e5c092fd80dbc6fe4ee1fd7119ed6461e1f0
Iggy <iggy.mw@gmail.com>
2013-07-10 23:29:49 -0500

    updated

---
+++ MVCDirectoryStructureCreator/MVCDirectoryStructureCreator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDirectoryStructureCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+            if(args[0].IsNullOrEmpty)
+            String projectName = path + "\\" + args[0];
+            String models = "Models";
+            String views = "Views";
+            String controllers = "Controllers";
+            //
+            // Create new folder 
+            //
+            Directory.CreateDirectory(projectName);
+            //
+            // Create more directory 
+            //
+            Directory.CreateDirectory(projectName + "\\" + models);
+            Directory.CreateDirectory(projectName + "\\" + views);
+            Directory.CreateDirectory(projectName + "\\" + controllers);
+            
+
+            
+        }
+    }
+}
